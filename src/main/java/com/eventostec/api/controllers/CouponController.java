97c703fcd3eef3935912602e2965c93a3af410ff
gablsl <gabrielsousadeveloper@gmail.com>
2024-10-17 19:28:10 -0300

    feat: implementing the coupon entity structure

---
+++ src/main/java/com/eventostec/api/controllers/CouponController.java
@@ -0,0 +1,28 @@
+package com.eventostec.api.controllers;
+
+import com.eventostec.api.domain.coupon.Coupon;
+import com.eventostec.api.domain.coupon.CouponRequestDTO;
+import com.eventostec.api.services.CouponService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestParam("/api/coupon")
+public class CouponController {
+
+    @Autowired
+    private CouponService couponService;
+
+    @PostMapping
+    public ResponseEntity<Coupon> addCoupon(@PathVariable UUID eventId, @RequestBody CouponRequestDTO data) {
+        Coupon coupon = couponService.addCouponToEvent(eventId, data);
+        return ResponseEntity.ok(coupon);
+    }
+}
---
+++ src/main/java/com/eventostec/api/services/CouponService.java
@@ -0,0 +1,40 @@
+package com.eventostec.api.services;
+
+import com.eventostec.api.domain.coupon.Coupon;
+import com.eventostec.api.domain.coupon.CouponRequestDTO;
+import com.eventostec.api.domain.event.Event;
+import com.eventostec.api.repositories.CouponRepository;
+import com.eventostec.api.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class CouponService {
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    public Coupon addCouponToEvent(UUID eventId, CouponRequestDTO data) {
+        Event event = eventRepository.findById(eventId).orElseThrow(
+                () -> new IllegalArgumentException("Event not found"));
+
+        Coupon coupon = new Coupon();
+        coupon.setCode(data.code());
+        coupon.setDiscount(data.discount());
+        coupon.setValidity(new Date(data.validity()));
+        coupon.setEvent(event);
+
+        return couponRepository.save(coupon);
+    }
+
+    public List<Coupon> consultCoupons(UUID eventId, Date currentDate) {
+        return couponRepository.findByEventIdAndValidityAfter(eventId, currentDate);
+    }
+}
